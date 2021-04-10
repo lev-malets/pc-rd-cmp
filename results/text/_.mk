55b6adcce8eadd53cb418f880622de7f6ab11e5e
Lev Malets <lev-malets@outlook.com>
2021-04-11 01:24:22 +0300

    fix report rule

--- results/text/_.mk
+++ results/text/_.mk
@@ -2,25 +2,25 @@ include make/base.mk
 
 $(DONE): $(TMP_DIR)/repo/main.pdf
 
-tex_files := $(patsubst $(DIR)/%,$(TMP_DIR)/repo/%,$(shell find $(DIR) -name '*.tex'))
+tex_files := $(patsubst $(DIR)/%,$(TMP_DIR)/repo/%,$(shell find $(DIR) -name '*.tex' -o -name '*.bib'))
+
+xelatex := xelatex -jobname=main main.tex
 
 define cmd
 $(TMP_DIR)/template_done:
 	$(call clone_repo, https://github.com/Kakadu/matmex-diploma-template, master, dfa74307f5c43905ecab56c11c6c069923bb6e5b)
 
 	touch $$@
+
+$(tex_files): $(TMP_DIR)/repo/%: | $(DIR)/% $(TMP_DIR)/template_done
+	ln -s $(WDIR)/$$(patsubst $(TMP_DIR)/repo/%,$(DIR)/%,$$@) $(TMP_DIR)/repo/
 endef
 
-$(TMP_DIR)/repo/main.pdf: $(tex_files) $(TMP_DIR)/repo/bib.bib
+$(TMP_DIR)/repo/main.pdf: $(tex_files)
 	cd $(dir $@) \
-	&& xelatex main.tex \
+	&& $(xelatex) \
 	&& bibtex main.aux \
-	&& xelatex main.tex \
-	&& xelatex main.tex
-
-$(TMP_DIR)/repo/%.tex: $(DIR)/%.tex $(TMP_DIR)/template_done
-	cp -f $< $@
-$(TMP_DIR)/repo/bib.bib: $(DIR)/bib.bib $(TMP_DIR)/template_done
-	cp -f $< $@
+	&& $(xelatex) \
+	&& $(xelatex)
 
 $(eval $(cmd))
