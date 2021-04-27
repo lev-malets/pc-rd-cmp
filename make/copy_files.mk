d7d9e1ce3d15ef641a39eacf6a1b30e83ce2dd7d
Lev Malets <lev-malets@outlook.com>
2021-04-27 19:25:23 +0300

    update report

--- make/copy_files.mk
+++ make/copy_files.mk
@@ -2,6 +2,7 @@
 TMP_DIR := $(TMP_DIR)/_
 
 src_files := $(patsubst $(DIR)/%,$(TMP_DIR)/%,$(shell find $(DIR) -maxdepth 1 -mindepth 1 ! -name '_.mk'))
+dep_files := $(patsubst $(DIR)/%,$(TMP_DIR)/%,$(shell find $(DIR) ! -name '_.mk'))
 
 define cmd
 $(src_files): $(TMP_DIR)/%: | $(DIR)/%
